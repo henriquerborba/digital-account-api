55ab550d5aebb5e98d93a707edc0244f6467d227
henriquerborba <henriquedeborba@hotmail.com>
2023-07-29 17:50:34 -0300

    refactor: Change in folder structure

--- src/main/java/com/henriqueborba/digital_account/account/Account.java
+++ src/main/java/com/henriqueborba/digital_account/account/Account.java
@@ -1,21 +1,10 @@
-package com.henriqueborba.digital_account.entities;
+package com.henriqueborba.digital_account.account;
+
+import com.henriqueborba.digital_account.customer.Customer;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
--- src/main/java/com/henriqueborba/digital_account/customer/Customer.java
+++ src/main/java/com/henriqueborba/digital_account/customer/Customer.java
@@ -1,15 +1,7 @@
-package com.henriqueborba.digital_account.entities;
+package com.henriqueborba.digital_account.customer;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
