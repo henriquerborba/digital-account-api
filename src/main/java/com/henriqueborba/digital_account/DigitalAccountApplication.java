fd6f5929196d8a2347da40d84fefe3e868c85b8c
henriquerborba <henriquedeborba@hotmail.com>
2023-07-29 11:32:36 -0300

    first commit

---
+++ src/main/java/com/henriqueborba/digital_account/DigitalAccountApplication.java
@@ -0,0 +1,13 @@
+package com.henriqueborba.digital_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAccountApplication.class, args);
+	}
+
+}
